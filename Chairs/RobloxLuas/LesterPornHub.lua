6eb83382cd889360ae5cff0ff38edbaa1331a9f4
MoistMonkey420 <76874951+MoistMonkey420@users.noreply.github.com>
2024-09-24 23:48:37 +0200

    Update LesterPornHub.lua

--- Chairs/RobloxLuas/LesterPornHub.lua
+++ Chairs/RobloxLuas/LesterPornHub.lua
@@ -132,7 +132,25 @@ local function getClosest(cframe)
     end
     if game.PlaceId == 2377868063 then
         disableAimbot = true
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/MoistMonkey420/Loader/refs/heads/main/Chairs/RobloxLuas/Strucid_Hitbox.lua"))()
+        _G.HeadSize = 20
+         
+        _G.Disabled = true
+
+        game:GetService('RunService').RenderStepped:connect(function()
+            if _G.Disabled then
+                for i,v in next, game:GetService('Players'):GetPlayers() do
+                    if v.Name ~= game:GetService('Players').LocalPlayer.Name then
+                        pcall(function()
+                            v.Character.HumanoidRootPart.Size = Vector3.new(_G.HeadSize,_G.HeadSize,_G.HeadSize)
+                            v.Character.HumanoidRootPart.Transparency = 0.7
+                            v.Character.HumanoidRootPart.BrickColor = BrickColor.new("Really black")
+                            v.Character.HumanoidRootPart.Material = "Neon"
+                            v.Character.HumanoidRootPart.CanCollide = false
+                        end)
+                    end
+                end
+            end
+        end)
     else
         disableAimbot = false
     end
@@ -332,7 +350,7 @@ local function ESP(plr)
                         Colorize(plr.TeamColor.Color)
                     end
                     Visibility(true, library)
-                else
+                else 
                     Visibility(false, library)
                 end
             else 
